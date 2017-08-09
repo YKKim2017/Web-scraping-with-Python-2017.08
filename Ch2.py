37711cd7d3655c4a7d43a15cbab3ad88d7bfbed2
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-09 16:39:33 +0900

    Update Ch2.py

--- Ch2.py
+++ Ch2.py
@@ -9,6 +9,6 @@ for name in nameList:
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bs0bj = BeautifulSoup(html, "html.parser")
 
-for sibling in bs0bj.find("table",{"id":"giftlist"}).tr.next_siblings:
+for sibling in bs0bj.find("table",{"id":"giftList"}).tr.next_siblings:
     print(sibling)
     
