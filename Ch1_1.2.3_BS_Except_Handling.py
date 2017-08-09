9e584ccd851efe3957e0147339b2482ca00e86a2
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-09 13:39:04 +0900

    Update Ch1_1.2.3_BS_Except_Handling.py

--- Ch1_1.2.3_BS_Except_Handling.py
+++ Ch1_1.2.3_BS_Except_Handling.py
@@ -1,1 +1,14 @@
 from urllib.request import urlopen
+from urllib.request import HTTPError
+from bs4 import BeautifulSoup
+
+try:
+    html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+except HTTPError as e:
+    print(e)
+    return None
+try:
+    badContent = bs0bj
+else:
+    bs0bj = BeautifulSoup(html.read(), "html.parser")
+    print(bs0bj.h1)
