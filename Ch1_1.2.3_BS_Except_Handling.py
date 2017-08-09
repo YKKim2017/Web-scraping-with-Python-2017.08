f708876723f2bbba17512d7aad4caa31781cb09f
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-09 13:43:42 +0900

    Update Ch1_1.2.3_BS_Except_Handling.py

--- Ch1_1.2.3_BS_Except_Handling.py
+++ Ch1_1.2.3_BS_Except_Handling.py
@@ -2,13 +2,20 @@ from urllib.request import urlopen
 from urllib.request import HTTPError
 from bs4 import BeautifulSoup
 
-try:
-    html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-except HTTPError as e:
-    print(e)
-    return None
-try:
-    badContent = bs0bj
-else:
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
     bs0bj = BeautifulSoup(html.read(), "html.parser")
-    print(bs0bj.h1)
+    title = bs0bj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
