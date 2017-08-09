3a740f0913a4a0d3ea41ca16b68eb2a5400b4066
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-09 16:26:50 +0900

    Update Ch2.py

--- Ch2.py
+++ Ch2.py
@@ -5,3 +5,9 @@ bs0bj = BeautifulSoup(html, "html.parser")
 nameList = bs0bj.findAll("span", {"class":"green"}) #bs0bj.findAll(class_="green") #bs0bj.findAll("", class_="green")
 for name in nameList:
     print(name.get_text())
+
+html = urlopen("http://pythonscraping.com/pages/page3.html")
+bs0bj = BeautifulSoup(html, "html.parser")
+for sibling in bs0bj.find("table", {"if":"giftlist"}).tr.next_siblings:
+    print(sibling)
+    
