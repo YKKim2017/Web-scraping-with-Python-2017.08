37ae0fc40018969c4d7fa928d252fa6498fca10c
YKKim2017 <30789697+YKKim2017@users.noreply.github.com>
2017-08-09 16:20:36 +0900

    Update Ch2.py

--- Ch2.py
+++ Ch2.py
@@ -1,1 +1,7 @@
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://pythonscraping.com/pages/warandpeace.html")
+bs0bj = BeautifulSoup(html, "html.parser")
+nameList = bs0bj.findAll("span", {"class":"green"}) #bs0bj.findAll(class_="green") #bs0bj.findAll("", class_="green")
+for name in nameList:
+    print(name.get_text())
